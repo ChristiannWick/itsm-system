13c7f6937add745ec810da157e994b2f9e182501
wick <casixremz@gmail.com>
2026-04-19 21:27:49 +0800

    add ticket

---
+++ itsm-frontend/src/api/ticket.api.js
@@ -0,0 +1,10 @@
+// import api from 'axios'
+import api from '@/api/axios'
+
+export const getTickets = (params = {}) => {
+    return api.get('/tickets', { params })
+}
+
+export const createTicket = (payload) => {
+    return api.post('/tickets', payload)
+}
--- itsm-frontend/src/views/TicketListView.vue
+++ itsm-frontend/src/views/TicketListView.vue
@@ -2,8 +2,17 @@
     <div>
         <h2>Tickets</h2>
 
-        <button @click="loadTickets">Load Tickets</button>
+        <!-- Create Ticket Form -->
+        <form @submit.prevent="submit">
+            <input v-model="form.title" placeholder="Title" />
+            <textarea v-model="form.description" placeholder="Description"></textarea>
+            
+            <button>Create Ticket</button>
+        </form>
 
+        <hr>
+
+        <!-- Ticket List -->
         <ul>
             <li v-for="ticket in tickets" :key="ticket.id">
                 {{ ticket.title }} - {{ ticket.status }}
@@ -17,19 +26,34 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import api from'@/api/axios'
+import { getTickets, createTicket } from '@/api/ticket.api'
 import { useAuthStore } from '@/stores/auth.store'
 import { useRouter } from 'vue-router'
 
 const tickets = ref([])
-
-const loadTickets = async () => {
-    const {data} = await api.get('/tickets')
-    tickets.value = data.data
-}
+const form = ref({
+    title: '',
+    description: '',
+})
 
 const auth = useAuthStore()
 const router = useRouter()
+
+const loadTickets = async () => {
+    const {data} = await getTickets()
+    tickets.value = data.data
+}
+
+const submit = async () => {
+    const { data } = await createTicket(form.value)
+
+    console.log(data, 'data in submit')
+
+    tickets.value.unshift(data)
+
+    form.value.title = ''
+    form.value.description = ''
+}
 
 const logout = () => {
     auth.logout()
